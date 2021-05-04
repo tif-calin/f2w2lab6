dec65fc8445b9d0c2b8357bb6a69c0455dd9bff9
tif-calin <culitif@tuta.io>
2021-05-04 14:40:30 -0700

    update package.json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // import our app file
-import app from './lib/app.js';
+import app from './lib/app.js'; 
 
 // either use the env variable PORT, or default to 8001
 const port = process.env.PORT || 8001;
